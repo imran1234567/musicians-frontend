82681eb0abd06a681e63f64086bd4846c56b6086
development-claymind <palash@claymindsolutions.com>
2023-07-05 11:12:00 +0530

    updated

---
+++ build/js/custom.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+  "use strict";
+
+  // ===========Right Sidebar============
+  $('[data-toggle="offcanvas"]').on("click", function () {
+    $("body").toggleClass("toggled");
+  });
+
+  // ===========Tooltip============
+  $('[data-toggle="tooltip"]').tooltip();
+
+  // ===========Single Items Slider============
+});
+// $(document).ready(function() {
+//     "use strict";
+
+//     // ===========Right Sidebar============
+//     $('[data-toggle="offcanvas"]').on('click', function() {
+//         $('body').toggleClass('toggled');
+//     });
+
+//     // ===========Tooltip============
+//     $('[data-toggle="tooltip"]').tooltip()
+
+//     // ===========Single Items Slider============
+
+// });
--- src/app/components/web/views/home/TopArrival/index.js
+++ src/app/components/web/views/home/TopArrival/index.js
@@ -1,165 +1,291 @@
+// import React, { Component } from "react";
+// import b2 from "./../../../../../../images/b2.jpg";
+// import a8 from "./../../../../../../images/a8.jpg";
+// import a7 from "./../../../../../../images/a7.jpg";
+// import b1 from "./../../../../../../images/b1.jpg";
+// import a3 from "./../../../../../../images/a3.jpg";
+// import a10 from "./../../../../../../images/a10.jpg";
+// import a9 from "./../../../../../../images/a9.jpg";
+// import pr11 from "./../../../../../../images/pr11.jpg";
+// import a1 from "./../../../../../../images/a1.jpg";
+
+// export default class TopArrival extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <section class="new-arrival">
+//           <div class="container-fluid">
+//             <h2 class="sec-title">Top Deals & New Arrivals</h2>
+//             <div class="new-arrival-list">
+//               <div class="row new-arrival-content">
+//                 <div class="col-lg-6 col-md-6 col-12 av-box">
+//                   <div class="row h-100">
+//                     <div class="col-lg-6 col-md-6 col-12">
+//                       <div class="new-arrival-box">
+//                         <div class="arrival-image">
+//                           <img src={a1} alt="a1" />
+//                         </div>
+//                         <div class="arrival-text">
+//                           <h6>Alesis Strike Multi 9-Pad Percussion Pad</h6>
+//                           <h5>$1,195.00</h5>
+//                         </div>
+//                       </div>
+//                     </div>
+//                     <div class="col-lg-6 col-md-6 col-12">
+//                       <div class="new-arrival-box">
+//                         <div class="arrival-image">
+//                           <img src={pr11} alt="pr11" />
+//                         </div>
+//                         <div class="arrival-text">
+//                           <h6>
+//                             Ibanez AW54CE OPN Artwood Acoustic Electric Guitar
+//                           </h6>
+//                           <h5>$595.00</h5>
+//                         </div>
+//                       </div>
+//                     </div>
+//                   </div>
+//                 </div>
+//                 <div class="col-lg-6 col-md-6 col-12 av-image">
+//                   <div class="arrival-image">
+//                     <img src={a9} alt="a9" />
+//                   </div>
+//                 </div>
+//               </div>
+
+//               <div class="row new-arrival-content">
+//                 <div class="col-lg-6 col-md-6 col-12 av-box">
+//                   <div class="row h-100">
+//                     <div class="col-lg-6 col-md-6 col-12">
+//                       <div class="new-arrival-box">
+//                         <div class="arrival-image">
+//                           <img src={a10} alt="a10" />
+//                         </div>
+//                         <div class="arrival-text">
+//                           <h6>Alesis Strike Multi 9-Pad Percussion Pad</h6>
+//                           <h5>$1,195.00</h5>
+//                         </div>
+//                       </div>
+//                     </div>
+//                     <div class="col-lg-6 col-md-6 col-12">
+//                       <div class="new-arrival-box">
+//                         <div class="arrival-image">
+//                           <img src={a3} alt="a3" />
+//                         </div>
+//                         <div class="arrival-text">
+//                           <h6>
+//                             Ibanez AW54CE OPN Artwood Acoustic Electric Guitar
+//                           </h6>
+//                           <h5>$595.00</h5>
+//                         </div>
+//                       </div>
+//                     </div>
+//                   </div>
+//                 </div>
+//                 <div class="col-lg-6 col-md-6 col-12 av-image">
+//                   <div class="arrival-image">
+//                     <img src={a10} alt="a10" />
+//                   </div>
+//                 </div>
+//               </div>
+
+//               <div class="row new-arrival-content">
+//                 <div class="col-lg-6 col-md-6 col-12 av-box">
+//                   <div class="row h-100">
+//                     <div class="col-lg-6 col-md-6 col-12">
+//                       <div class="new-arrival-box">
+//                         <div class="arrival-image">
+//                           <img src={a3} alt="a3" />
+//                         </div>
+//                         <div class="arrival-text">
+//                           <h6>Alesis Strike Multi 9-Pad Percussion Pad</h6>
+//                           <h5>$1,195.00</h5>
+//                         </div>
+//                       </div>
+//                     </div>
+//                     <div class="col-lg-6 col-md-6 col-12">
+//                       <div class="new-arrival-box">
+//                         <div class="arrival-image">
+//                           <img src={b1} alt="b1" />
+//                         </div>
+//                         <div class="arrival-text">
+//                           <h6>
+//                             Ibanez AW54CE OPN Artwood Acoustic Electric Guitar
+//                           </h6>
+//                           <h5>$595.00</h5>
+//                         </div>
+//                       </div>
+//                     </div>
+//                   </div>
+//                 </div>
+//                 <div class="col-lg-6 col-md-6 col-12 av-image">
+//                   <div class="arrival-image">
+//                     <img src={b1} alt="b1" />
+//                   </div>
+//                 </div>
+//               </div>
+
+//               <div class="row new-arrival-content">
+//                 <div class="col-lg-6 col-md-6 col-12 av-box">
+//                   <div class="row h-100">
+//                     <div class="col-lg-6 col-md-6 col-12">
+//                       <div class="new-arrival-box">
+//                         <div class="arrival-image">
+//                           <img src={a7} alt="a7" />
+//                         </div>
+//                         <div class="arrival-text">
+//                           <h6>Alesis Strike Multi 9-Pad Percussion Pad</h6>
+//                           <h5>$1,195.00</h5>
+//                         </div>
+//                       </div>
+//                     </div>
+//                     <div class="col-lg-6 col-md-6 col-12">
+//                       <div class="new-arrival-box">
+//                         <div class="arrival-image">
+//                           <img src={a8} alt="a8" />
+//                         </div>
+//                         <div class="arrival-text">
+//                           <h6>
+//                             Ibanez AW54CE OPN Artwood Acoustic Electric Guitar
+//                           </h6>
+//                           <h5>$595.00</h5>
+//                         </div>
+//                       </div>
+//                     </div>
+//                   </div>
+//                 </div>
+//                 <div class="col-lg-6 col-md-6 col-12 av-image">
+//                   <div class="arrival-image2">
+//                     <img src={b2} alt="b2" />
+//                   </div>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </section>
+//       </div>
+//     );
+//   }
+// }
+
 import React, { Component } from "react";
+import axios from "axios";
+import {Link} from 'react-router-dom';
 import b2 from "./../../../../../../images/b2.jpg";
-import a8 from "./../../../../../../images/a8.jpg";
-import a7 from "./../../../../../../images/a7.jpg";
 import b1 from "./../../../../../../images/b1.jpg";
-import a3 from "./../../../../../../images/a3.jpg";
 import a10 from "./../../../../../../images/a10.jpg";
 import a9 from "./../../../../../../images/a9.jpg";
-import pr11 from "./../../../../../../images/pr11.jpg";
-import a1 from "./../../../../../../images/a1.jpg";
 
 export default class TopArrival extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [],
+      error: null,
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://13.233.106.34:4000/api/product/getAllproductList")
+      .then((response) => {
+        const products = response.data.product.filter(
+          (product) => product.featured === true
+        );
+        this.setState({ products });
+      })
+      .catch((error) => {
+        this.setState({ error: error.message });
+      });
+  }
+
   render() {
+    const { products, error } = this.state;
+    const images = [a9, a10, b1, b2];
+
+    if (error) {
+      return <div>Error: {error}</div>;
+    }
+
     return (
       <div>
-        <section class="new-arrival">
-          <div class="container-fluid">
-            <h2 class="sec-title">Top Deals & New Arrivals</h2>
-            <div class="new-arrival-list">
-              <div class="row new-arrival-content">
-                <div class="col-lg-6 col-md-6 col-12 av-box">
-                  <div class="row h-100">
-                    <div class="col-lg-6 col-md-6 col-12">
-                      <div class="new-arrival-box">
-                        <div class="arrival-image">
-                          <img src={a1} alt="a1" />
-                        </div>
-                        <div class="arrival-text">
-                          <h6>Alesis Strike Multi 9-Pad Percussion Pad</h6>
-                          <h5>$1,195.00</h5>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col-lg-6 col-md-6 col-12">
-                      <div class="new-arrival-box">
-                        <div class="arrival-image">
-                          <img src={pr11} alt="pr11" />
-                        </div>
-                        <div class="arrival-text">
-                          <h6>
-                            Ibanez AW54CE OPN Artwood Acoustic Electric Guitar
-                          </h6>
-                          <h5>$595.00</h5>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-lg-6 col-md-6 col-12 av-image">
-                  <div class="arrival-image">
-                    <img src={a9} alt="a9" />
-                  </div>
-                </div>
-              </div>
+        <section className="new-arrival">
+          <div className="container-fluid">
+            <h2 className="sec-title">Top Deals & New Arrivals</h2>
+            <div className="new-arrival-list">
+              {products.map((product, index) => {
+                const productIndex = index % products.length;
+                const imageIndex = index % images.length;
+                const nextProductIndex = (index + 1) % products.length;
 
-              <div class="row new-arrival-content">
-                <div class="col-lg-6 col-md-6 col-12 av-box">
-                  <div class="row h-100">
-                    <div class="col-lg-6 col-md-6 col-12">
-                      <div class="new-arrival-box">
-                        <div class="arrival-image">
-                          <img src={a10} alt="a10" />
+                if (productIndex % 2 === 0) {
+                  return (
+                    <div className="row new-arrival-content" key={productIndex}>
+                      <div className="col-lg-6 col-md-6 col-12 av-box">
+                        <div className="row h-100">
+                          <div className="col-lg-6 col-md-6 col-12">
+                            <div className="new-arrival-box">
+                              <div className="arrival-image">
+                              <Link
+                                    to={{
+                                      pathname: `/p/${product.slug}/${product.id}`,
+                                      state: product,
+                                    }}
+                                  >
+                                <img src={product.photo} alt={product.name} /></Link>
+                              </div>
+                              <div className="arrival-text">
+                              <Link
+                                    to={{
+                                      pathname: `/p/${product.slug}/${product.id}`,
+                                      state: product,
+                                    }}
+                                  >
+                                <h6>{product.name}</h6></Link>
+                                <h5>${product.price}</h5>
+                              </div>
+                            </div>
+                          </div>
+                          {nextProductIndex < products.length && (
+                            <div className="col-lg-6 col-md-6 col-12">
+                              <div className="new-arrival-box">
+                                <div className="arrival-image">
+                                <Link
+                                    to={{
+                                      pathname: `/p/${product.slug}/${product.id}`,
+                                      state: product,
+                                    }}
+                                  >
+                                  <img
+                                    src={products[nextProductIndex].photo}
+                                    alt={products[nextProductIndex].name}
+                                  /></Link>
+                                </div>
+                                <div className="arrival-text">
+                                <Link
+                                    to={{
+                                      pathname: `/p/${product.slug}/${product.id}`,
+                                      state: product,
+                                    }}
+                                  >
+                                  <h6>{products[nextProductIndex].name}</h6></Link>
+                                  <h5>${products[nextProductIndex].price}</h5>
+                                </div>
+                              </div>
+                            </div>
+                          )}
                         </div>
-                        <div class="arrival-text">
-                          <h6>Alesis Strike Multi 9-Pad Percussion Pad</h6>
-                          <h5>$1,195.00</h5>
+                      </div>
+                      <div className="col-lg-6 col-md-6 col-12 av-image">
+                        <div className="arrival-image">
+                          <img src={images[imageIndex]} alt={product.name} />
                         </div>
                       </div>
                     </div>
-                    <div class="col-lg-6 col-md-6 col-12">
-                      <div class="new-arrival-box">
-                        <div class="arrival-image">
-                          <img src={a3} alt="a3" />
-                        </div>
-                        <div class="arrival-text">
-                          <h6>
-                            Ibanez AW54CE OPN Artwood Acoustic Electric Guitar
-                          </h6>
-                          <h5>$595.00</h5>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-lg-6 col-md-6 col-12 av-image">
-                  <div class="arrival-image">
-                    <img src={a10} alt="a10" />
-                  </div>
-                </div>
-              </div>
-
-              <div class="row new-arrival-content">
-                <div class="col-lg-6 col-md-6 col-12 av-box">
-                  <div class="row h-100">
-                    <div class="col-lg-6 col-md-6 col-12">
-                      <div class="new-arrival-box">
-                        <div class="arrival-image">
-                          <img src={a3} alt="a3" />
-                        </div>
-                        <div class="arrival-text">
-                          <h6>Alesis Strike Multi 9-Pad Percussion Pad</h6>
-                          <h5>$1,195.00</h5>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col-lg-6 col-md-6 col-12">
-                      <div class="new-arrival-box">
-                        <div class="arrival-image">
-                          <img src={b1} alt="b1" />
-                        </div>
-                        <div class="arrival-text">
-                          <h6>
-                            Ibanez AW54CE OPN Artwood Acoustic Electric Guitar
-                          </h6>
-                          <h5>$595.00</h5>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-lg-6 col-md-6 col-12 av-image">
-                  <div class="arrival-image">
-                    <img src={b1} alt="b1" />
-                  </div>
-                </div>
-              </div>
-
-              <div class="row new-arrival-content">
-                <div class="col-lg-6 col-md-6 col-12 av-box">
-                  <div class="row h-100">
-                    <div class="col-lg-6 col-md-6 col-12">
-                      <div class="new-arrival-box">
-                        <div class="arrival-image">
-                          <img src={a7} alt="a7" />
-                        </div>
-                        <div class="arrival-text">
-                          <h6>Alesis Strike Multi 9-Pad Percussion Pad</h6>
-                          <h5>$1,195.00</h5>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col-lg-6 col-md-6 col-12">
-                      <div class="new-arrival-box">
-                        <div class="arrival-image">
-                          <img src={a8} alt="a8" />
-                        </div>
-                        <div class="arrival-text">
-                          <h6>
-                            Ibanez AW54CE OPN Artwood Acoustic Electric Guitar
-                          </h6>
-                          <h5>$595.00</h5>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-lg-6 col-md-6 col-12 av-image">
-                  <div class="arrival-image2">
-                    <img src={b2} alt="b2" />
-                  </div>
-                </div>
-              </div>
+                  );
+                }
+                return null;
+              })}
             </div>
           </div>
         </section>
