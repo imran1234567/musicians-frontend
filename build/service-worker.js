b853cfc298396ab4abf3b34a4644f329c096803b
Imran <imran@wsegames.com>
2023-07-06 10:53:52 +0530

    update

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8c3a751fe27179641cb920d83af8815f.js"
+  "/precache-manifest.6d622a2efaec16ce5e3f8a3523b75231.js"
 );
 
 self.addEventListener('message', (event) => {
